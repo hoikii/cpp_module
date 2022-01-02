008776310cdfc6961a8e2531f226d52635b49808
kanlee <kanlee@student.42seoul.kr>
2022-01-03 01:58:35 +0900

    cpp00-ex00 supports multibyte char

---
+++ cpp_module_00/ex00/megaphone.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kanlee <kanlee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/01 23:01:19 by kanlee            #+#    #+#             */
+/*   Updated: 2022/01/03 01:53:04 by kanlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <locale>
+#include <cstring>
+#include <cstdlib>
+
+using std::endl;
+
+/* convert each **argv to wide char
+ * to support multibyte characters like öõóó or αβγδε.
+*/
+void prn_upper(const char *str)
+{
+	wchar_t wch;
+
+	int len = std::strlen(str);
+	while (1)
+	{
+		int ret = std::mbtowc(&wch, str, len);
+		if (ret <= 0)
+			break ;
+		std::wcout << (wchar_t)towupper(wch);
+		str += ret;
+		len -= ret;
+	}
+}
+
+int main(int ac, char **av)
+{
+	std::setlocale(LC_ALL, "en_US.utf8");
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		return 0;
+	}
+	while (*(++av))
+		prn_upper(*av);
+	std::wcout << endl;
+	return 0;
+}
